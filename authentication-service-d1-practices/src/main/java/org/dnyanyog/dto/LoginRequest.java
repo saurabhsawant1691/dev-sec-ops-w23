21cc59dfe73dc036d901ae450641deaa8f71844c
_saurabhh_sawant <saurabhsawant2001@gmail.com>
2024-03-08 21:31:07 +0530

    feat: Apply regular expressions
    * Apply regular expression for password

--- authentication-service-d1-practices/src/main/java/org/dnyanyog/dto/LoginRequest.java
+++ authentication-service-d1-practices/src/main/java/org/dnyanyog/dto/LoginRequest.java
@@ -2,10 +2,14 @@ package org.dnyanyog.dto;
 
 import org.springframework.stereotype.Component;
 
+import jakarta.validation.constraints.Pattern;
+
 @Component
 public class LoginRequest { // DTO => Data Transfer Object
 							// View
 	private String username="Admin";
+	@Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*[@_\\-]).{8,}$",
+	        message = "Invalid password")
 	private String password="Admin123";
 
 	public String getUsername() {
